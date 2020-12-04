0e40fd88805ded9969d1af17e9045934e8690aa0
Khai Do <zaro0508@gmail.com>
2020-12-03 19:41:50 -0800

    slight upddate

--- sc_cost_meter/app.py
+++ sc_cost_meter/app.py
@@ -22,8 +22,8 @@ def get_time_period_yesterday():
 def lambda_handler(event, context):
   synapse_ids = utils.get_marketplace_synapse_ids()
   log.debug(f'customers list: {synapse_ids}')
-  failures = 0
-  success = 0
+  num_customers = len(synapse_ids)
+  num_failed_reports = 0
   for synapse_id in synapse_ids:
     customer_info = utils.get_marketplace_customer_info(synapse_id)
     log.debug(f'marketplace customer info: {customer_info}')
@@ -36,15 +36,16 @@ def lambda_handler(event, context):
     cost, unit = utils.get_customer_cost(customer_id, yesterday, "DAILY")
     status, result = utils.report_cost(cost, customer_id, product_code)
     if status == 'Failed':
-      failures = failures + 1
+      num_failed_reports = num_failed_reports + 1
       log.info(f'Failed to report cost for product {product_code} to customer {customer_id}')
     else:
-      record_id = result['MeteringRecordId']
+      metering_record_id = result['MeteringRecordId']
       log.info(f'Successfully reported cost for product {product_code}'
-                f'to customer {customer_id} with record id {record_id}')
-      success = success + 1
+                f'to customer {customer_id} with record id {metering_record_id}')
 
-  message = f'Metering processed: failed reports ({failures}), successful reports ({success})'
+  num_success_reports = num_customers - num_failed_reports
+  message = f'Metering processed: failed reports ({num_failed_reports}),' \
+            f'successful reports ({num_success_reports})'
   log.info(message)
   response = {
     "statusCode": 200,
@@ -52,4 +53,4 @@ def lambda_handler(event, context):
       "message": message,
     }),
   }
-  return response
+
